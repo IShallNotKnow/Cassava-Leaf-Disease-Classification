b0d60907b84e11a299441b35ce3aff711c95e67a
Anish <aajunk42@gmail.com>
2026-08-09 14:34:57 -0700

    Models uploaded to HF hub for quick loading

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import gradio as gr
 import torch
-
 from inference import TTA, load_model
+from huggingface_hub import hf_hub_download
+
+student_weights = hf_hub_download(
+    repo_id="IShallNotKnow/cassava-models",
+    filename="Modified_EffNet_f1_saved_model.pth"
+)
 
 device = torch.device("cpu")
-model = load_model("EffNet/results/Modified_EffNet_f1_saved_model.pth", device)
+model = load_model(student_weights, device)
 tta = TTA(model, device, n_aug=5)
 
 def classify(image):
